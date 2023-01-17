63e4c7643a12842ae592bd5b26d497ea2d97b2a8
GBfds <95057840+GBfds@users.noreply.github.com>
2023-01-17 09:30:07 -0300

    font end

--- src/_document.tsx
+++ src/_document.tsx
@@ -7,7 +7,7 @@ export default class MyDocument extends Document{
             <Html>
                 <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;700&display=swap" rel="stylesheet"/>
+                <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet"/>
                 <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
                 </Head>
                 <body>
